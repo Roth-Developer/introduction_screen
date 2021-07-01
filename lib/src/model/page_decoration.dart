aaa5258526de45ed0d49937d3cb1b37295f7d13a
Doung sereyroth <doungsereyrath@gmail.com>
2021-07-01 18:41:46 +0700

    title textstyle 60

--- lib/src/model/page_decoration.dart
+++ lib/src/model/page_decoration.dart
@@ -66,7 +66,7 @@ class PageDecoration {
   const PageDecoration({
     this.pageColor,
     this.titleTextStyle = const TextStyle(
-      fontSize: 30.0,
+      fontSize: 60.0,
       fontWeight: FontWeight.bold,
     ),
     this.bodyTextStyle = const TextStyle(
